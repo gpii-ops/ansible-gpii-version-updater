d7deb40c01625a6d759333940ee4052cd33f29b4
Tyler Roscoe <tyler@cryptio.net>
2017-09-19 16:49:16 -0600

    Update tests to testinfra 1.6 style. Pin ansible and molecule to compatible versions.

--- tests/test_default.py
+++ tests/test_default.py
@@ -5,9 +5,9 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_docker_container_runs(Command, Sudo):
-    with Sudo():
-        cmd = Command("docker ps")
+def test_docker_container_runs(host):
+    with host.sudo():
+        cmd = host.command("docker ps")
     assert cmd.rc == 0
     # We don't want the version-updater to really run and push changes during
     # testing. Fortunately, the container won't get far because (in testing) it
@@ -15,9 +15,9 @@ def test_docker_container_runs(Command, Sudo):
     assert "gpii/version-updater" in cmd.stdout
 
 
-def test_ssh_known_hosts_configured(File, Sudo):
-    with Sudo():
-        ff = File("/home/deploy/gpii-ci-ssh/known_hosts")
+def test_ssh_known_hosts_configured(host):
+    with host.sudo():
+        ff = host.file("/home/deploy/gpii-ci-ssh/known_hosts")
         # Existence check seems superfluous but it produces a more helpful
         # error message than .contains() when file does not exist.
         assert ff.exists
